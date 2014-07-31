d61bb79ad43b3b2ec0ff9edfb6437af3d02eba6c
Alex Codreanu <alexcodreanu86@gmail.com>
2014-07-30 22:04:16 -0500

    finished prime-factors kata

--- prime-factors/spec/prime_factors/core_spec.clj
+++ prime-factors/spec/prime_factors/core_spec.clj
@@ -2,6 +2,16 @@
   (:require [speclj.core :refer :all]
             [prime-factors.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "prime-factors"
+  (it "returns [] for 1"
+    (should= [] (prime-factors 1)))
+  (it "returns [2] for 2"
+    (should= [2](prime-factors 2)))
+  (it "returns [3] for 3"
+    (should= [3] (prime-factors 3)))
+  (it "returns [2 2] for 4"
+    (should= [2 2] (prime-factors 4)))
+  (it "returns [3 3] for 9"
+    (should= [3 3] (prime-factors 9)))
+  (it "returns [2 3 5 7] for 210"
+    (should= [2 3 5 7] (prime-factors 210))))
--- prime-factors/src/prime_factors/core.clj
+++ prime-factors/src/prime_factors/core.clj
@@ -1,1 +1,13 @@
 (ns prime-factors.core)
+
+(defn prime-factors [x]
+  (loop [remainder x
+         factors []
+         next-factor 2]
+    (if (< remainder next-factor)
+      factors
+      (if (zero? (mod remainder next-factor))
+        (recur (/ remainder next-factor)
+               (conj factors next-factor)
+               next-factor)
+        (recur remainder factors (inc next-factor))))))
